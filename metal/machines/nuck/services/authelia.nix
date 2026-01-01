2cedfe0c78ed8c0b4154a9cc6ed1c6368ce58fbd
Dylan McTiernan <dylan@mctiernan.io>
2026-01-01 14:20:50 -0500

    Use variable for client secret

--- metal/machines/nuck/services/authelia.nix
+++ metal/machines/nuck/services/authelia.nix
@@ -11,6 +11,7 @@ in {
       storageEncryptionKeyFile = config.sops.secrets."nuck/authelia/storage_encryption_key".path;
       oidcHmacSecretFile = config.sops.secrets."nuck/authelia/oidc_hmac_secret".path;
       oidcIssuerPrivateKeyFile = config.sops.secrets."nuck/authelia/oidc_rsa_private_key".path;
+      forgejoClientSecret = config.sops.secrets."nuck/authelia/forgejo_client_secret".path;
     };
 
     settings = {
@@ -88,7 +89,7 @@ in {
           {
             client_id = "forgejo";
             client_name = "Forgejo";
-            client_secret = "$plaintext$b87067421779d30d7ba8a78a4028fe3c0105eb433f16612bb37fc39866f4b43b";
+            client_secret = "$plaintext$$${forgejo_client_secret}";
             public = false;
             authorization_policy = "one_factor";
 
