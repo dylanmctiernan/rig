5e1976965d7a4b2dab186d839056fd655cb6d011
Dylan McTiernan <dylan@mctiernan.io>
2026-01-02 11:46:47 -0500

    Fix Authelia users.yml tmpfiles syntax error
    Move users.yml content from tmpfiles.rules to environment.etc and copy
    in preStart script. tmpfiles doesn't support multi-line file content.

--- metal/machines/nuck/services/authelia.nix
+++ metal/machines/nuck/services/authelia.nix
@@ -142,19 +142,29 @@ in {
   # Ensure Authelia data directory exists with correct permissions
   systemd.tmpfiles.rules = [
     "d ${authelia.dataDir} 0750 authelia-main authelia-main -"
-    # Create users.yml file directly in /data/authelia using tmpfiles
-    ''f ${authelia.dataDir}/users.yml 0600 authelia-main authelia-main - users:
-  dylan:
-    displayname: "Dylan McTiernan"
-    email: dylan@mctiernan.io
-    password: "$pbkdf2-sha512$310000$c8p78n7pUMln0jzvd4aK4Q$JNRBzwAo0ek5qKn50cFzzvE9RfXStRzS9/P6HVWtgzLsKHD6G0rWJFJ0E8OTZxU9zfPGgznS6RqJWH/3r6Qv3Q"
-    groups:
-      - admins
-''
   ];
 
-  # Override serviceConfig to allow writing to /data directory
+  # Create users.yml file declaratively in /etc, then copy to dataDir
+  environment.etc."authelia/users.yml" = {
+    text = ''
+      users:
+        dylan:
+          displayname: "Dylan McTiernan"
+          email: dylan@mctiernan.io
+          password: "$pbkdf2-sha512$310000$c8p78n7pUMln0jzvd4aK4Q$JNRBzwAo0ek5qKn50cFzzvE9RfXStRzS9/P6HVWtgzLsKHD6G0rWJFJ0E8OTZxU9zfPGgznS6RqJWH/3r6Qv3Q"
+          groups:
+            - admins
+    '';
+    mode = "0600";
+  };
+
+  # Copy users.yml to dataDir and override serviceConfig
   systemd.services.authelia-main = {
+    preStart = ''
+      cp -f /etc/authelia/users.yml ${authelia.dataDir}/users.yml
+      chown authelia-main:authelia-main ${authelia.dataDir}/users.yml
+      chmod 0600 ${authelia.dataDir}/users.yml
+    '';
     serviceConfig = {
       ReadWritePaths = [ authelia.dataDir ];
       # Relax some systemd hardening to allow /data access
