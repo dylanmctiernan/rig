b0897090b181c204ae6b2b6dfebb35054816960c
Dylan McTiernan <dylan@mctiernan.io>
2026-01-04 23:06:00 -0500

    Use internal TLS certs for Caddy

--- metal/machines/nuck/services/caddy.nix
+++ metal/machines/nuck/services/caddy.nix
@@ -4,6 +4,7 @@
     enable = true;
     virtualHosts."${vars.services.kanidm.fqdn}" = {
       extraConfig = ''
+        tls internal
         reverse_proxy https://127.0.0.1:${toString vars.services.kanidm.port} {
           transport http {
             tls_insecure_skip_verify
